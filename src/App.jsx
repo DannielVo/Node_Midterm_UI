72dce8cae5a5d4303db75a819bc9999ff00ea1b3
DannielVo <phattinhoc2017@gmail.com>
2025-12-12 15:21:50 +0700

    Feat: Set up routes in App.jsx and install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <div>Chat App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
