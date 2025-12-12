431b7177993253f96286e8125e838e4c1d711281
DannielVo <phattinhoc2017@gmail.com>
2025-12-12 15:42:55 +0700

    Feat: Install boostrap and import Nunito font family

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <>
+    <Container className="text-secondary">
       <Routes>
         <Route path="/" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </Container>
   );
 };
 
